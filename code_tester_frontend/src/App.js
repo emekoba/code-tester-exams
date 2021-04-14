1dcce4b97329c6509db7699c3b2af908c98a162b
emekoba <klojett.re@gmail.com>
2021-04-14 16:34:33 +0100

    initialised app

---
+++ code_tester_frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Test1 from "./Test1/Test1";
+
+function App() {
+	return (
+		<div className="App">
+			<Test1 />
+		</div>
+	);
+}
+
+export default App;
---
+++ code_tester_frontend/src/Test1/Test1.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Test1() {
+	return <div>this is the first test</div>;
+}
+
+export default Test1;
